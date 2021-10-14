cf8cbc9753d616d90183e130933eddbfe9e28ced
Andrzej Pruszyński <andrzej.pruszynski90@gmail.com>
2021-10-14 14:51:04 +0200

    ex/angular-1 -

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,7 @@ import { HeroService } from '../hero.service';
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
-})
+}) 
 
 export class HeroesComponent implements OnInit {
 
@@ -25,5 +25,18 @@ export class HeroesComponent implements OnInit {
       .subscribe(heroes => this.heroes = heroes);
   }
   
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h != hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 
 }
